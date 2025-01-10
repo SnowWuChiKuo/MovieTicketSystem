ac7659b7037968f6605eeaebc6a205d85c46f6d4
Wei <afduje@gmail.com>
2025-01-10 15:35:12 +0800

    前台MovieController製作中

---
+++ ClientSide/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientSide.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Movie(int movieId)
+        {
+            return View();
+        }
+    }
+}
